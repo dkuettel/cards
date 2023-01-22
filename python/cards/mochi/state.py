79e9f2ad6af50ff2ba97eec6640bffd64f043a7a
Daniel Kuettel <dkuettel@gmail.com>
2023-01-22 19:02:26 +0100

    make diff cound and iterator explicit

--- python/cards/mochi/state.py
+++ python/cards/mochi/state.py
@@ -2,36 +2,29 @@ from __future__ import annotations
 
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
+from typing import Iterator
 
 from cards.mochi.deck import MochiCard, MochiDeck
-from cards.tools import IteratorWithLength
 
 MochiState = dict[str, MochiCard]
 
 
 def states_from_apply_diff(
     deck: MochiDeck, state: MochiState, diff: MochiDiff
-) -> IteratorWithLength[MochiState]:
-    def g():
-        nonlocal state
-        for id in diff.removed:
-            deck.delete_card(id)
-            state.pop(id)
-            yield state
-        for c in diff.changed:
-            u = deck.update_card(c)
-            state[u.id] = u
-            yield state
-        for c in diff.new_cards:
-            u = deck.create_card(c.get_content())
-            c.update_on_disk(u.id)
-            state[u.id] = u
-            yield state
-
-    return IteratorWithLength(
-        g(),
-        len(diff.removed) + len(diff.changed) + len(diff.new_cards),
-    )
+) -> Iterator[MochiState]:
+    for id in diff.removed:
+        deck.delete_card(id)
+        state.pop(id)
+        yield state
+    for c in diff.changed:
+        u = deck.update_card(c)
+        state[u.id] = u
+        yield state
+    for c in diff.new_cards:
+        u = deck.create_card(c.get_content())
+        c.update_on_disk(u.id)
+        state[u.id] = u
+        yield state
 
 
 @dataclass(frozen=True)
@@ -48,6 +41,9 @@ class MochiDiff:
         changed = [c for c in target.values() if c != state[c.id]]
         assert set(target) <= set(state)
         return cls(removed, changed, new_cards)
+
+    def __len__(self):
+        return len(self.removed) + len(self.changed) + len(self.new_cards)
 
 
 class NewMochiCard(ABC):
