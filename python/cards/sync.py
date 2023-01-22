174fc60449814d1f89ecad4a66a94aec76f07dca
Daniel Kuettel <dkuettel@gmail.com>
2023-01-22 19:09:53 +0100

    redone sync

--- python/cards/sync.py
+++ python/cards/sync.py
@@ -3,11 +3,16 @@ from __future__ import annotations
 from pathlib import Path
 
 import click
-from cards.documents import get_all_documents
+from cards.data.plain.documents import get_all_documents
+from cards.data.plain.transform import (
+    get_cards_from_documents,
+    get_new_cards_from_documents,
+)
+from cards.mochi.deck import MochiDeck
+from cards.mochi.state import MochiDiff, states_from_apply_diff
 from tqdm import tqdm
 
-from cards import mochi as M
-from cards.cards import Card, NewCard, get_cards, get_new_cards
+# TODO strange, above isort doesnt seem to understand project and 3rd party imports?
 
 # TODO python 3.10 has good pprint for dataclasses too
 # from pprint import pprint
@@ -30,60 +35,35 @@ from cards.cards import Card, NewCard, get_cards, get_new_cards
 @click.command()
 def main(source: Path = Path("brainscape/v2")):
 
-    authentication = M.Authentication("55a09ced245dd6fde6780896")
-    # deck_id = "VetSe1ij"  # brainscape
-    deck_id = "f1Xj0eiR"  # brainscape2
+    token = "-"
+    # deck_id = "-"  # brainscape
+    deck_id = "-"  # brainscape2
+    deck = MochiDeck.from_token(deck_id, token)
 
     documents = get_all_documents(source)
-    m_cards = M.list_cards(authentication, deck_id)
 
-    new_cards = get_new_cards(documents)
-    print(f"{len(new_cards)} new cards to add.")
-    for new_card in tqdm(new_cards, desc="add new cards"):
-        nc = new_card_from_new_card(new_card, deck_id)
-        c = M.create_card(authentication, nc)
-        new_card.update_on_disk(c.id)
+    new_cards = get_new_cards_from_documents(documents)
+    cards = get_cards_from_documents(documents)
+
+    # TODO have this local as well, when/how to rescan to be sure?
+    state = {c.id: c for c in deck.list_cards()}
+    target = {c.id: c for c in cards}
+
+    diff = MochiDiff.from_targets(state, target, new_cards)
+    diff.print_summary()
+
+    if len(diff) > 0:
+        for state in tqdm(
+            states_from_apply_diff(deck, state, diff), total=len(diff), desc="sync"
+        ):
+            # TODO should write state to file everytime
+            pass
 
     # TODO cache yes, but also missing deletion, so need to get full list anyway
     # and probably want to move the doc meta update (when prompt disappears) to documents?
     # and explicitly call it, just like we update disk here explicity on NewCard
 
-    cards = get_cards(documents)
-    print(f"{len(cards)} to potentially update.")
-    for card in tqdm(cards, desc="update cards"):
-        c = card_from_card(card, deck_id)
-        M.update_card(authentication, c)
-    # TODO not giving a stats now for how much actually changed
-    # print(f"{changed}/{len(cards)} actual changes.")
-
-    known_ids = {i for doc in documents for i in doc.meta.ids()}
-    mochi_ids = {i.id for i in m_cards}
-    # TODO here could also double check both directions if ids match up
-    ids_to_delete = mochi_ids - known_ids
-    print(f"{len(ids_to_delete)} cards to delete on mochi.")
-    for card_id in tqdm(ids_to_delete, desc="delete cards"):
-        # TODO there is also an option to set trash to True
-        # is it then not listed anymore in this deck? I think it's listed
-        # so manual stuff will be listed, since we dont parse the trashed yet
-        # that's a bit inconsistent
-        M.delete_card(authentication, card_id)
-
-
-def card_from_card(card: Card, deck_id: str) -> M.Card:
-    return M.Card(
-        id=card.id,
-        content=card.content,
-        deck_id=deck_id,
-        review_reverse=False,
-    )
-
-
-def new_card_from_new_card(new_card: NewCard, deck_id: str) -> M.NewCard:
-    return M.NewCard(
-        content=new_card.content,
-        deck_id=deck_id,
-        review_reverse=False,
-    )
+    # TODO wasnt there also a field trashed? need to deal with it
 
 
 # TODO how to use pyright globally as a linter
