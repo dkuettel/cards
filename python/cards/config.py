e13b5c4a18780f98eb46ec4add73cf0eaee7b1ab
Daniel Kuettel <dkuettel@gmail.com>
2023-01-22 19:32:05 +0100

    config and credentials in dot files

---
+++ python/cards/config.py
@@ -0,0 +1,40 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from pathlib import Path
+
+from serde import serde
+from serde.toml import from_toml
+
+
+@serde
+@dataclass
+class Sync:
+    path: Path
+    deck_id: str
+
+
+@serde
+@dataclass
+class Config:
+    sync: Sync
+
+    @classmethod
+    def from_default_file(cls):
+        return from_toml(cls, Path(".config.toml").read_text())
+
+
+@serde
+@dataclass
+class Mochi:
+    token: str
+
+
+@serde
+@dataclass
+class Credentials:
+    mochi: Mochi
+
+    @classmethod
+    def from_default_file(cls):
+        return from_toml(cls, Path(".credentials.toml").read_text())
