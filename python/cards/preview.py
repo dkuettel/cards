3ca1b70803c4e728249570bce3d69af5f7d51d66
Daniel Kuettel <dkuettel@gmail.com>
2023-02-10 18:08:18 +0100

    fix preview after self-contained, katex local and pandoc template

--- python/cards/preview.py
+++ python/cards/preview.py
@@ -2,62 +2,9 @@ from pathlib import Path
 from subprocess import CalledProcessError, run
 from typing import Optional
 
-from flask import Flask, current_app, redirect, render_template_string, url_for
+from flask import Flask, current_app, redirect, url_for
 
 app = Flask(__name__)
-
-
-# NOTE most of <head> is copied from 'pandoc --standalone' to make it work with math
-card_template = """
-<!doctype html>
-<html>
-<head>
-  <title>preview</title>
-  <meta charset="utf-8" />
-  <meta name="generator" content="pandoc" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes" />
-  <title>test</title>
-  <style type="text/css">
-      code{white-space: pre-wrap;}
-      span.smallcaps{font-variant: small-caps;}
-      span.underline{text-decoration: underline;}
-      div.column{display: inline-block; vertical-align: top; width: 50%;}
-  </style>
-  <script src="https://cdn.jsdelivr.net/npm/katex@0.16.4/dist/katex.min.js"></script>
-  <script>document.addEventListener("DOMContentLoaded", function () {
-    var mathElements = document.getElementsByClassName("math");
-    for (var i = 0; i < mathElements.length; i++) {
-      var texText = mathElements[i].firstChild;
-      if (mathElements[i].tagName == "SPAN") { katex.render(texText.data, mathElements[i], { displayMode: mathElements[i].classList.contains("display"), throwOnError: false } );
-    }}});</script>
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.4/dist/katex.min.css" />
-</head>
-<body>
-
-  preview of {{name}}
-
-  <hr/>
-
-  <div style="width: 25em; font-size: 1.5em">
-    {{preview|safe}}
-  </div>
-
-  <script>
-    setInterval(() => {
-      fetch('/mtime')
-        .then(response => response.json())
-        .then(data => {
-          if(data["mtime"]>{{mtime}}) { location.reload(); }
-        })
-        .catch(error => {
-          console.error(error);
-        });
-    }, 500); // milliseconds
-  </script>
-
-</body>
-</html>
-"""
 
 
 @app.route("/")
@@ -72,29 +19,33 @@ def preview():
     if path is None:
         return "n/a"
     stat = path.stat()
+    name = path.relative_to(current_app.config["watch_folder"])
     try:
         result = run(
             [
                 "pandoc",
                 str(path),
-                "--katex=https://cdn.jsdelivr.net/npm/katex@0.16.4/dist/",
-                "--metadata=pagetitle=preview",
+                # needs to be local, because --self-contained copies it everytime
+                # (using https://cdn.jsdelivr.net/npm/katex@0.16.4/dist/ will rate-limit)
+                "--katex=/home/dkuettel/bin/nodejs/lib/node_modules/katex/dist/",
+                f"--metadata=pagetitle={name}",
                 # to find images relative to the markdown file
                 f"--resource-path={path.parent}",
-                # embeds images, maybe more, but the whole katex js lib is not embedded
+                # embeds images, and even katex
+                # plus output is a full html document
+                # (otherwise it's a html sub-tree)
                 "--self-contained",
+                # adapted from 'pandoc -D html'
+                "--template=python/cards/preview-template.html",
+                f"--variable=preview_name:{name}",
+                f"--variable=mtime:{stat.st_mtime}",
                 "--to=html",
             ],
             check=True,
             text=True,
             capture_output=True,
         )
-        return render_template_string(
-            card_template,
-            preview=result.stdout,
-            mtime=stat.st_mtime,
-            name=path.relative_to(current_app.config["watch_folder"]),
-        )
+        return result.stdout
     except CalledProcessError as e:
         return e.stdout + e.stderr
 
