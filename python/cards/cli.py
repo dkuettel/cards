4bd855205b903e7db9b41ffc3c1abf9f2e7ecbcb
Daniel Kuettel <dkuettel@gmail.com>
2023-09-06 23:45:03 +0200

    remove convert code

--- python/cards/cli.py
+++ python/cards/cli.py
@@ -39,28 +39,3 @@ def preview():
     config = Config.from_test_flag(state.test)
 
     main(config.sync.path)
-
-
-@app.command()
-def convert():
-    import json
-    from pathlib import Path
-
-    from cards.data.markdown import Meta, write_meta
-
-    source = Path("data/rich")
-    target = Path("data/markdown")
-    target.mkdir(parents=True, exist_ok=True)
-
-    metas = {}
-
-    for path in source.glob("*/content.md"):
-        meta = json.loads((path.parent / "meta.json").read_text())
-        name = path.parent.name
-        target_path = target / f"{name}.md"
-        metas[target_path.absolute().relative_to(target.absolute())] = Meta(
-            meta["forward"], meta["backward"]
-        )
-        target_path.write_text(path.read_text())
-
-    write_meta(target, metas)
